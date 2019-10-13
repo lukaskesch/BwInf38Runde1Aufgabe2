772beebf0302b2b9f5f777a593d19d9a72a75712
Lukas Kesch <Lukas.Kesch@hotmail.de>
2019-10-13 20:52:49 +0200

    Created Method "Recursion"

--- BwInfAufgabe2/MainWindow.xaml.cs
+++ BwInfAufgabe2/MainWindow.xaml.cs
@@ -29,14 +29,13 @@ namespace BwInfAufgabe2
         {
             try
             {
-                string Eingabe = TextboxEingabe.Text;
-                int[] Zahl = new int[Eingabe.Length];
+                string Input = TextboxEingabe.Text;
+                int[] DigitsLeft = new int[Input.Length * 2];
+                int[] DigitsChosen = new int[Input.Length * 2];
 
-                for (int i = 0; i < Zahl.Length; i++)
-                {
-                    Zahl[i] = int.Parse(Eingabe[i].ToString());
-                }
+                DeclareArrays(DigitsLeft, DigitsChosen, Input);
 
+                Recursion(DigitsLeft, DigitsChosen, 0);
 
             }
             catch (Exception)
@@ -44,6 +43,98 @@ namespace BwInfAufgabe2
 
                 throw;
             }
+
+        }
+        private void DeclareArrays(int[] DigitsLeft, int[] DigitsChoose, string Input)
+        {
+            for (int i = 0; i < Input.Length; i++)
+            {
+                DigitsLeft[i] = int.Parse(Input[i].ToString());
+            }
+
+            for (int i = Input.Length; i < DigitsLeft.Length; i++)
+            {
+                DigitsLeft[i] = -2;
+            }
+
+            for (int i = 0; i < DigitsChoose.Length; i++)
+            {
+                DigitsChoose[i] = -2;
+            }
+        }
+        private void Recursion(int[] DigitsLeft, int[] DigitsChosen, int NumberOfSteps)
+        {
+            //Wenn keine Zahlen mehr übrig sind
+            if (DigitsLeft[0] == -2)
+            {
+                CheckResult(DigitsChosen, true);
+            }
+            //Wenn nur noch eine oder 3 Zahlen vorhanden sind
+            else if ((DigitsLeft[1] == -1) || (DigitsLeft[3] == -1 && DigitsLeft[2] != -1))
+            {
+                return;
+            }
+            else
+            {
+                //Choose - Gehe um Schritt nach vorne
+                GeheSchritte(ref DigitsLeft, ref DigitsChosen, NumberOfSteps);
+
+                //Check - Schaue ob Zwischenergebnis ein unnötiger Pfad ist
+                CheckResult(DigitsChosen, false);
+
+                //Explore - Probiere alle möglichen kommenden Schritte aus
+                for (int i = 2; i <= 4; i++)
+                {
+                    Recursion(DigitsLeft, DigitsChosen, i);
+                }
+
+                //Un-choose - Gehe diesen Schritt wieder zurück
+                GeheSchritteZurueck(ref DigitsLeft, ref DigitsChosen, NumberOfSteps);
+            }
+        }
+
+        private bool CheckResult(int[] DigitsChosen, bool Finished)
+        {
+            return true;
+        }
+        private int GetFirstFreeIndex(int[] Array)
+        {
+            for (int i = 0; i < Array.Length; i++)
+            {
+                if (Array[i] == -2)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        private void GeheSchritte(ref int[] DigitsLeft, ref int[] DigitsChosen, int NumberOfSteps)
+        {
+            int Index = GetFirstFreeIndex(DigitsChosen);
+
+            //Übertrage die Zahlen von "Zahlen" nach "Choosen"
+            if (NumberOfSteps >= 2)
+            {
+                DigitsChosen[Index] = DigitsLeft[0];
+                DigitsChosen[Index + 1] = DigitsLeft[1];
+            }
+            else if (NumberOfSteps >= 3)
+            {
+                DigitsChosen[Index + 2] = DigitsLeft[2];
+            }
+            else if (NumberOfSteps == 4)
+            {
+                DigitsChosen[Index + 3] = DigitsLeft[3];
+            }
+
+            //Ziehe Zahl um Schritte nach vorne
+
+        }
+        private void GeheSchritteZurueck(ref int[] DigitsLeft, ref int[] DigitsChosen, int NumberOfSteps)
+        {
+            //Zahlenarray um Schritte nach hinten verschieben
+
+            //Ende von Chosen in Zahlenarray einfügen
         }
     }
 }
