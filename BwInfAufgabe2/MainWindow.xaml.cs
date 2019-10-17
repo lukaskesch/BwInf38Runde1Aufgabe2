b5fd9f1ec09891dea78db794c80e471f8484cc6f
Lukas Kesch <Lukas.Kesch@hotmail.de>
2019-10-17 21:46:12 +0200

    Values in array are being moved up and down

--- BwInfAufgabe2/MainWindow.xaml.cs
+++ BwInfAufgabe2/MainWindow.xaml.cs
@@ -24,18 +24,14 @@ namespace BwInfAufgabe2
         {
             InitializeComponent();
         }
-
+        int BestNumberOfZeros = 10000000;
         private void ButtonRechnen_Click(object sender, RoutedEventArgs e)
         {
             try
             {
-                string Input = TextboxEingabe.Text;
-                int[] DigitsLeft = new int[Input.Length * 2];
-                int[] DigitsChosen = new int[Input.Length * 2];
 
-                DeclareArrays(DigitsLeft, DigitsChosen, Input);
 
-                Recursion(DigitsLeft, DigitsChosen, 0);
+
 
             }
             catch (Exception)
@@ -43,7 +39,12 @@ namespace BwInfAufgabe2
 
                 throw;
             }
+            string Input = TextboxEingabe.Text;
+            int[] DigitsLeft = new int[Input.Length * 2];
+            int[] DigitsChosen = new int[Input.Length * 2];
+            DeclareArrays(DigitsLeft, DigitsChosen, Input);
 
+            Recursion(DigitsLeft, DigitsChosen, 0);
         }
         private void DeclareArrays(int[] DigitsLeft, int[] DigitsChoose, string Input)
         {
@@ -69,8 +70,8 @@ namespace BwInfAufgabe2
             {
                 CheckResult(DigitsChosen, true);
             }
-            //Wenn nur noch eine oder 3 Zahlen vorhanden sind
-            else if ((DigitsLeft[1] == -1) || (DigitsLeft[3] == -1 && DigitsLeft[2] != -1))
+            //Wenn nur noch eine Zahlen vorhanden ist
+            else if (DigitsLeft[1] == -2)
             {
                 return;
             }
@@ -80,7 +81,10 @@ namespace BwInfAufgabe2
                 GeheSchritte(ref DigitsLeft, ref DigitsChosen, NumberOfSteps);
 
                 //Check - Schaue ob Zwischenergebnis ein unnötiger Pfad ist
-                CheckResult(DigitsChosen, false);
+                if (!CheckResult(DigitsChosen, false))
+                {
+                    return;
+                }
 
                 //Explore - Probiere alle möglichen kommenden Schritte aus
                 for (int i = 2; i <= 4; i++)
@@ -95,6 +99,19 @@ namespace BwInfAufgabe2
 
         private bool CheckResult(int[] DigitsChosen, bool Finished)
         {
+            if (GetNumberOfZeros(DigitsChosen) > BestNumberOfZeros)
+            {
+                return false;
+            }
+            if (Finished)
+            {
+                int NumberOfZeros = GetNumberOfZeros(DigitsChosen);
+                if (NumberOfZeros < BestNumberOfZeros)
+                {
+                    BestNumberOfZeros = NumberOfZeros;
+                    SetAusgabe(DigitsChosen, BestNumberOfZeros);
+                }
+            }
             return true;
         }
         private int GetFirstFreeIndex(int[] Array)
@@ -108,12 +125,47 @@ namespace BwInfAufgabe2
             }
             return -1;
         }
+        private int GetNumberOfZeros(int[] DigitsChosen)
+        {
+            int NumberOfZeros = 0;
+            for (int i = 0; i < DigitsChosen.Length - 1; i++)
+            {
+                if (DigitsChosen[i] == -1 && DigitsChosen[i + 1] == 0)
+                {
+                    NumberOfZeros++;
+                }
+            }
+            return NumberOfZeros;
+        }
+        private void SetAusgabe(int[] DigitsChosen, int BestNumberOfZeros)
+        {
+            string Ausgabe = string.Empty;
+            for (int i = 0; i < DigitsChosen.Length; i++)
+            {
+                if (DigitsChosen[i] == -2)
+                {
+                    break;
+                }
+                if (DigitsChosen[i] == -1)
+                {
+                    Ausgabe += "-";
+                    continue;
+                }
+                Ausgabe += DigitsChosen[i];
+            }
+            TextboxAusgabe.Text = Ausgabe;
+            MessageBox.Show(BestNumberOfZeros.ToString() + " Zahlenbloecke beginnen mit einer Null");
+        }
         private void GeheSchritte(ref int[] DigitsLeft, ref int[] DigitsChosen, int NumberOfSteps)
         {
             int Index = GetFirstFreeIndex(DigitsChosen);
 
-            //Übertrage die Zahlen von "Zahlen" nach "Choosen"
-            if (NumberOfSteps >= 2)
+            //Übertrage die Zahlen von "Zahlen" nach "Chosen"
+            if (NumberOfSteps == 0)
+            {
+                return;
+            }
+            else if (NumberOfSteps >= 2)
             {
                 DigitsChosen[Index] = DigitsLeft[0];
                 DigitsChosen[Index + 1] = DigitsLeft[1];
@@ -128,13 +180,62 @@ namespace BwInfAufgabe2
             }
 
             //Ziehe Zahl um Schritte nach vorne
+            for (int i = 0; i < DigitsLeft.Length - NumberOfSteps; i++)
+            {
+                DigitsLeft[i] = DigitsLeft[i + NumberOfSteps];
+            }
+
+            //Lösche Ende von Zahlen
+            Index = GetFirstFreeIndex(DigitsLeft) - 1;
+            if (Index < 0)
+            {
+                return;
+            }
+            for (int i = Index; i > Index - NumberOfSteps; i--)
+            {
+                DigitsLeft[i] = -2;
+            }
 
         }
         private void GeheSchritteZurueck(ref int[] DigitsLeft, ref int[] DigitsChosen, int NumberOfSteps)
         {
             //Zahlenarray um Schritte nach hinten verschieben
+            for (int i = DigitsLeft.Length - 1; i >= 0 + NumberOfSteps; i--)
+            {
+                DigitsLeft[i] = DigitsLeft[i - NumberOfSteps];
+            }
 
             //Ende von Chosen in Zahlenarray einfügen
+            int Index = GetFirstFreeIndex(DigitsChosen);
+            if (NumberOfSteps == 0) { return; }
+            if (NumberOfSteps == 4)
+            {
+                DigitsLeft[3] = DigitsChosen[Index - 1];
+                DigitsLeft[2] = DigitsChosen[Index - 2];
+                DigitsLeft[1] = DigitsChosen[Index - 3];
+                DigitsLeft[0] = DigitsChosen[Index - 4];
+
+                DigitsChosen[Index - 4] = -2;
+                DigitsChosen[Index - 3] = -2;
+            }
+            else if (NumberOfSteps == 3)
+            {
+                DigitsLeft[2] = DigitsChosen[Index - 1];
+                DigitsLeft[1] = DigitsChosen[Index - 2];
+                DigitsLeft[0] = DigitsChosen[Index - 3];
+
+                DigitsChosen[Index - 3] = -2;
+            }
+            else if (NumberOfSteps == 2)
+            {
+                if (Index - 2 < 0) { return; }
+
+                DigitsLeft[1] = DigitsChosen[Index - 1];
+                DigitsLeft[0] = DigitsChosen[Index - 2];
+
+            }
+            DigitsChosen[Index - 1] = -2;
+            DigitsChosen[Index - 2] = -2;
         }
     }
 }
